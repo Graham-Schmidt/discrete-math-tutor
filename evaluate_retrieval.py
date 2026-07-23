e4a1773c2858945c4d370b6483e59e5f3f34eb84
HamBoneGS <graham.n.schmidt@gmail.com>
2026-07-22 21:14:12 -0500

    basic evaluation loop for retrieved chunks

---
+++ evaluate_retrieval.py
@@ -0,0 +1,23 @@
+import chromadb
+from openai import OpenAI
+
+from eval.retrieval_eval import evaluate_retrieval
+from store import get_collection
+from config import TEST_COLLECTION_NAME, CHROMA_PERSIST_DIR
+from test_queries import TEST_QUERIES
+
+
+def main():
+    chroma_client = chromadb.PersistentClient(CHROMA_PERSIST_DIR)
+    openai_client = OpenAI()
+    collection = get_collection(
+        client=chroma_client, collection_name=TEST_COLLECTION_NAME
+    )
+    for query in TEST_QUERIES:
+        evaluate_retrieval(
+            query=query, collection=collection, openai_client=openai_client
+        )
+
+
+if __name__ == "__main__":
+    main()
